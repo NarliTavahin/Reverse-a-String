b2ab8f1a03c78169edf346eb8934b9fc01322b8a
Narli Tavahin <121438793+NarliTavahin@users.noreply.github.com>
2023-11-04 16:32:45 +0330

    upload files

---
+++ index-1.js
@@ -0,0 +1,16 @@
+function Submit(){  
+    var str = document.getElementById("name").value;  
+    document.getElementById("reverse").value = reverseString(str);  
+    }  
+ 
+
+    function reverseString(str) {
+
+        let newString = "";
+        for (let i = str.length - 1; i >= 0; i--) {
+            newString += str[i];
+        }
+        return newString;
+    }
+    
+
